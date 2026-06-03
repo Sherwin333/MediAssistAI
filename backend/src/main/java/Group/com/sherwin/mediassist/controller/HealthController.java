751718d53bb0e1b25f9f4e649ff29705ac14221a
Sherwin333 <sherwinashton33@gmail.com>
2026-06-03 12:19:22 +0530

    Third Commit

--- backend/src/main/java/Group/com/sherwin/mediassist/controller/HealthController.java
+++ backend/src/main/java/Group/com/sherwin/mediassist/controller/HealthController.java
@@ -1,9 +1,11 @@
 package Group.com.sherwin.mediassist.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class HealthController {
 
     @GetMapping("/health")

ccdc2c734398ff01708b22d01a16f53f7f356cc7
Sherwin333 <sherwinashton33@gmail.com>
2026-06-03 11:48:08 +0530

    First Commit

---
+++ backend/src/main/java/Group/com/sherwin/mediassist/MediassistApplication.java
@@ -0,0 +1,13 @@
+package Group.com.sherwin.mediassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediassistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediassistApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/Group/com/sherwin/mediassist/MediassistApplicationTests.java
@@ -0,0 +1,13 @@
+package Group.com.sherwin.mediassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediassistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
